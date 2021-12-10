04b63b2c92d8a254053da6376b1cff8b7322d296
dianagbr <70939874+dianagbr@users.noreply.github.com>
2021-12-10 17:25:57 +0200

    Add files via upload

---
+++ js/accordion.js
@@ -0,0 +1,18 @@
+// Accordion - Source: https://codepen.io/kathykato/pen/MoZJom //
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+    console.log('toggle')
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
---
+++ js/slider.js
@@ -0,0 +1,27 @@
+// Carousel - Source: https://codepen.io/tepiho4219/pen/OJxXeQP
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoWidth: true,
+  stagePadding: 0,
+  margin: 50,
+  dots: false,
+  nav: true,
+  navText: [
+    "<span id='prev'></span>",
+    "<span id='next'><img src='../assets/arrow-right.svg' alt='arrow'/></span>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    450: {
+      items: 1,
+    },
+    960: {
+      items: 2,
+    },
+    1200: {
+      items: 2,
+    },
+  },
+});
